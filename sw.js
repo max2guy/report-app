4a217d2a01d81c6a881b421631f948d7c6b05d05
max2guy <max2guy@gmail.com>
2026-03-20 14:48:58 +0900

    fix: absentNames 복원 + loadHistorySeed 보완 업데이트 + 병합 시 absentNames 보존

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@ messaging.onBackgroundMessage(payload => {
   });
 });
 
-const CACHE_NAME = 'report-app-v29';
+const CACHE_NAME = 'report-app-v30';
 const APP_SHELL = [
   './',
   './index.html',
