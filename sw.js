28151995bedf0f8b9d01a6beaa8ab5faf845653f
max2guy <max2guy@gmail.com>
2026-03-20 14:03:12 +0900

    feat: 출결 통계에 교구(장년) 결석률 탭 추가 (SW v24)

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@ messaging.onBackgroundMessage(payload => {
   });
 });
 
-const CACHE_NAME = 'report-app-v23';
+const CACHE_NAME = 'report-app-v24';
 const APP_SHELL = [
   './',
   './index.html',
