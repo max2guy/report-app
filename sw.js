e92d12409b145bc08da82440ff3fbe060426fcd1
max2guy <max2guy@gmail.com>
2026-03-20 14:52:48 +0900

    feat: 보고서 앱 받은 메시지 개별 삭제 기능 추가

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@ messaging.onBackgroundMessage(payload => {
   });
 });
 
-const CACHE_NAME = 'report-app-v30';
+const CACHE_NAME = 'report-app-v31';
 const APP_SHELL = [
   './',
   './index.html',
