0c08b12b6ff43efd0b94da1e709cedc87b10c3b6
max2guy <max2guy@gmail.com>
2026-03-22 23:33:13 +0900

    style: 중고등부/청년부 카드 헤더 제목 두 줄 표시

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@ messaging.onBackgroundMessage(payload => {
   });
 });
 
-const CACHE_NAME = 'report-app-v45';
+const CACHE_NAME = 'report-app-v46';
 const APP_SHELL = [
   './',
   './index.html',
